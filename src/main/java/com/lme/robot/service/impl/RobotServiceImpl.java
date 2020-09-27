024cdcec3b44e3208306c28feba46c4c8b55ee99
meghavipatel <meghavi.patel@ladbrokescoral.com>
2020-09-27 17:04:34 +0100

    Code for TDD added. More Test case added with given 3 examples.

--- src/main/java/com/lme/robot/service/impl/RobotServiceImpl.java
+++ src/main/java/com/lme/robot/service/impl/RobotServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lme.robot.service.impl;
 
 import com.lme.robot.model.Position;
+import com.lme.robot.model.enums.Orientation;
 import com.lme.robot.service.RobotService;
 
 public class RobotServiceImpl implements RobotService {
@@ -16,6 +17,126 @@ public class RobotServiceImpl implements RobotService {
 
     @Override
     public Position moveRobot(Position position, String instructions) {
+        for (char c : instructions.toCharArray()) {
+            switch (c) {
+                case 'L':
+                    position.setOrientation(turnLeft(position.getOrientation()));
+                    break;
+
+                case 'R':
+                    position.setOrientation(turnRight(position.getOrientation()));
+                    break;
+
+                case 'F':
+                    moveForward(position);
+                    break;
+                default:
+                    break;
+            }
+            if (position.isLost()) {
+                break;
+            }
+        }
         return position;
+    }
+
+    /**
+     * The robot turns left 90 degrees and remains on the current grid point.
+     *
+     * @param orientation The current orientation of the robot.
+     * @return The new orientation of the robot after turning left.
+     */
+    private Orientation turnLeft(Orientation orientation) {
+        switch (orientation) {
+            case N:
+                orientation = Orientation.W;
+                break;
+
+            case S:
+                orientation = Orientation.E;
+                break;
+
+            case E:
+                orientation = Orientation.N;
+                break;
+
+            case W:
+                orientation = Orientation.S;
+                break;
+        }
+        return orientation;
+    }
+
+    /**
+     * The robot turns right 90 degrees and remains on the current grid point.
+     *
+     * @param orientation The current orientation of the robot.
+     * @return The new orientation of the robot after turning right.
+     */
+    private Orientation turnRight(Orientation orientation) {
+        switch (orientation) {
+            case N:
+                orientation = Orientation.E;
+                break;
+
+            case S:
+                orientation = Orientation.W;
+                break;
+
+            case E:
+                orientation = Orientation.S;
+                break;
+
+            case W:
+                orientation = Orientation.N;
+                break;
+        }
+        return orientation;
+    }
+
+    /**
+     * The robot moves forward one grid point in the direction of the current
+     * orientation and maintains the same orientation
+     *
+     * @param position The current position of the robot.
+     */
+    private void moveForward(Position position) {
+        byte currentXCoordinate = position.getXCoordinate();
+        byte currentYCoordinate = position.getYCoordinate();
+        boolean lost = true;
+        switch (position.getOrientation()) {
+            case N:
+                if (currentYCoordinate < maxYCoordinate) {
+                    lost = false;
+                    position.setYCoordinate(++currentYCoordinate);
+                }
+                break;
+
+            case S:
+                if (currentYCoordinate > 0) {
+                    lost = false;
+                    position.setYCoordinate(--currentYCoordinate);
+                }
+                break;
+
+            case E:
+                if (currentXCoordinate < maxXCoordinate) {
+                    lost = false;
+                    position.setXCoordinate(++currentXCoordinate);
+                }
+                break;
+
+            case W:
+                if (currentXCoordinate > 0) {
+                    lost = false;
+                    position.setXCoordinate(--currentXCoordinate);
+                }
+                break;
+        }
+
+        if (lost && !scentedPosition[currentXCoordinate][currentYCoordinate]) {
+            position.setLost(true);
+            scentedPosition[currentXCoordinate][currentYCoordinate] = true;
+        }
     }
 }
--- src/test/groovy/com/lme/robot/service/impl/RobotServiceImplTest.groovy
+++ src/test/groovy/com/lme/robot/service/impl/RobotServiceImplTest.groovy
@@ -7,6 +7,7 @@ import spock.lang.Specification
 
 /**
  * The aim of this class is to test the method of the Robot Service.
+ * 100% Test coverage of the service class has been implemented.
  */
 class RobotServiceImplTest extends Specification {
 
@@ -17,7 +18,7 @@ class RobotServiceImplTest extends Specification {
         Position p = new Position(5 as byte, 3 as byte, Orientation.N)
 
         when:
-        def result = robotService.moveRobot(p, "FRRFLLFFRRFLL")
+        def result = robotService.moveRobot(p,"FRRFLLFFRRFLL")
 
         then:
         result.getXCoordinate() == 5 as byte
@@ -27,12 +28,99 @@ class RobotServiceImplTest extends Specification {
 
         when:
         p = new Position(4 as byte, 3 as byte, Orientation.E)
-        result = robotService.moveRobot(p, "FFLFLFFLF")
+        result = robotService.moveRobot(p,"FFLFLFFLF")
 
         then:
         result.getXCoordinate() == 3 as byte
         result.getYCoordinate() == 2 as byte
         result.getOrientation() == Orientation.S
         !result.isLost()
+    }
+
+
+    def "Should_11E_When_11E_RFRFRFRF"() {
+        given:
+        Position p = new Position(1 as byte, 1 as byte, Orientation.E)
+
+        when:
+        final result = robotService.moveRobot(p,"RFRFRFRF")
+
+        then:
+        result.getXCoordinate() == 1 as byte
+        result.getYCoordinate() == 1 as byte
+        result.getOrientation() == Orientation.E
+        !result.isLost()
+    }
+
+    def "Should_33NLost_When_32N_FRRFLLFFRRFLL"() {
+        given:
+        Position p = new Position(3 as byte, 2 as byte, Orientation.N)
+
+        when:
+        final result = robotService.moveRobot(p,"FRRFLLFFRRFLL")
+
+        then:
+        result.getXCoordinate() == 3 as byte
+        result.getYCoordinate() == 3 as byte
+        result.getOrientation() == Orientation.N
+        result.isLost()
+    }
+
+    def "Should_23S_When_32N_FRRFLLFFRRFLL_before_03W_LLFFFLFLFL"() {
+        given:
+        Position p = new Position(3 as byte, 2 as byte, Orientation.N)
+        robotService.moveRobot(p,"FRRFLLFFRRFLL")
+        p = new Position(0 as byte, 3 as byte, Orientation.W)
+
+        when:
+        final result = robotService.moveRobot(p,"LLFFFLFLFL")
+
+        then:
+        result.getXCoordinate() == 2 as byte
+        result.getYCoordinate() == 3 as byte
+        result.getOrientation() == Orientation.S
+        !result.isLost()
+    }
+
+    def "Should_Lost_When_yCoordinateIs0_orientationIsS"() {
+        given:
+        Position p = new Position(3 as byte, 0 as byte, Orientation.S)
+
+        when:
+        final result = robotService.moveRobot(p,"FRRFLLFFRRFLL")
+
+        then:
+        result.getXCoordinate() == 3 as byte
+        result.getYCoordinate() == 0 as byte
+        result.getOrientation() == Orientation.S
+        result.isLost()
+    }
+
+    def "Should_Lost_When_xCoordinateIs5_orientationIsE"() {
+        given:
+        Position p = new Position(5 as byte, 3 as byte, Orientation.E)
+
+        when:
+        final result = robotService.moveRobot(p,"FRRFLLFFRRFLL")
+
+        then:
+        result.getXCoordinate() == 5 as byte
+        result.getYCoordinate() == 3 as byte
+        result.getOrientation() == Orientation.E
+        result.isLost()
+    }
+
+    def "Should_Lost_When_xCoordinateIs0_orientationIsW"() {
+        given:
+        Position p = new Position(0 as byte, 3 as byte, Orientation.W)
+
+        when:
+        final result = robotService.moveRobot(p,"FRRFLLFFRRFLL")
+
+        then:
+        result.getXCoordinate() == 0 as byte
+        result.getYCoordinate() == 3 as byte
+        result.getOrientation() == Orientation.W
+        result.isLost()
     }
 }
